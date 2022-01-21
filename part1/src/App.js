4047e9c1201b2f86fc1a2c1926eda6d43d08cc7f
tian <tianwu@outlook.com>
2022-01-22 00:13:25 +1100

    add part1 exercise1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   const Header = (props) => {
     return (
@@ -37,9 +39,9 @@ const App = () => {
   const Content = (props) => {
     return (
       <>
-        <Part part={props.part1} />
-        <Part part={props.part2} />
-        <Part part={props.part3} />
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
       </>
     );
   };
@@ -49,9 +51,9 @@ const App = () => {
       <>
         <p>
           Number of exercises{" "}
-          {props.part1.exercises +
-            props.part2.exercises +
-            props.part3.exercises}
+          {props.parts[0].exercises +
+            props.parts[1].exercises +
+            props.parts[2].exercises}
         </p>
       </>
     );
@@ -60,8 +62,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
