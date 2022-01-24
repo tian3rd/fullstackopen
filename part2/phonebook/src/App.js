e734f30e5360132174748945d1be528e8b16b935
tian <tianwu@outlook.com>
2022-01-24 22:22:25 +1100

    add part2 exercise2.7 - phone number

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,29 +3,39 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const addName = (name) => (event) => {
+  const addNameAndNumber = (name, phone) => (event) => {
     event.preventDefault();
     if (persons.map((person) => person.name).includes(name)) {
       // use a template literal to create a new error message
       alert(`${name} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name }));
+      // it's a dictionary object, but can be abbreviated to just variable name (or use name: name, phone: phone)
+      setPersons(persons.concat({ name, phone }));
     }
     // don't forget to clear the name input
     setNewName("");
+    setNewNumber("");
   };
 
   const showNewName = (event) => {
     setNewName(event.target.value);
   };
 
+  const showNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName(newName)}>
+      <form onSubmit={addNameAndNumber(newName, newNumber)}>
         <div>
           name: <input value={newName} onChange={showNewName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={showNewNumber} />
         </div>
         <div>
           <button type="submit">add</button>
