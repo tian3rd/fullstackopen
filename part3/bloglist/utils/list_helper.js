ef66f5a62474d35efef777e8a5d899c427a22c38
tian <tianwu@outlook.com>
2022-01-31 21:49:34 +1100

    add part4 exercise4.4 - test with describe

--- part3/bloglist/utils/list_helper.js
+++ part3/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((prev, curr) => {
+    return prev + curr.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
