98b9eedbff80e733afba85708c714686cc56fbd4
tian <tianwu@outlook.com>
2022-02-03 10:05:13 +1100

    add jest config

---
+++ part3/bloglist/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "node",
+};
+// equivalent to add the following in package.json (https://mongoosejs.com/docs/jest.html):
+// "jest": {
+//   "testEnvironment": "node"
+// }
